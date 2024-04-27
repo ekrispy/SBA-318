8a325a833f8c3b91962e38158381448eed40073e
ekrispy <estevan.crispin@gmail.com>
2024-04-27 11:55:20 -0600

    upated comments to filter comments by user id and add patch, and delete for interaction

--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const comments = require("../data/comments");
-const userComments = require("../data/comments");
 
 router
- .route("/")
- .get((req, res) => {
+.route("/")
+.get((req, res) => {
     res.json(comments); // Return comments data in JSON format
   })
- .post((req, res, next) => {
+.post((req, res, next) => {
     if (req.body.userId && req.body.mealId && req.body.comment) {
       const comment = {
         id: comments[comments.length - 1].id + 1,
@@ -23,15 +22,45 @@ router
       next({ status: 400, message: "Insufficient Data" }); // Pass error object to next middleware
     }
   });
-  router
- .route("/:id")
- .get((req, res, next) => {
-    const comments = userComments.find((comments) => comments.mealId === parseInt(req.params.id));
-    if (comments) {
-      res.json(comments);
+
+router
+.route("/:id")
+.get((req, res, next) => {
+    const userId = parseInt(req.params.id);
+    const userComments = comments.filter((comment) => comment.userId === userId);
+    if (userComments.length > 0) {
+      res.json(userComments);
     } else {
-      next({ status: 404, message: "Meal Not Found" });
+      const error = new Error("No comments found for this user");
+      error.status = 404;
+      return next(error);
+    }
+  })
+.patch((req, res, next) => {
+    const userId = parseInt(req.params.id);
+    const userComments = comments.filter((comment) => comment.userId === userId);
+    if (userComments.length > 0) {
+      const updatedComment = userComments[0];
+      updatedComment.comment = req.body.comment;
+      res.json(updatedComment);
+    } else {
+      const error = new Error("No comments found for this user");
+      error.status = 404;
+      return next(error);
+    }
+  })       
+.delete((req, res, next) => {
+    const userId = parseInt(req.params.id);
+    const userComments = comments.filter((comment) => comment.userId === userId);
+    if (userComments.length > 0) {
+      const index = comments.indexOf(userComments[0]);
+      comments.splice(index, 1);
+      res.json({ message: "Comment deleted successfully" });
+    } else {
+      const error = new Error("No comments found for this user");
+      error.status = 404;
+      return next(error);
     }
   });
-  
-  module.exports = router;
+
+module.exports = router;
